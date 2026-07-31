6c24a0c9b154cf3b4d8b7b8c8b2102185ba82997
Sebastian Livoni <seb@sl1.dk>
2026-07-31 13:05:59 +0200

    WebAssembly

--- coolprop-sys/build.rs
+++ coolprop-sys/build.rs
@@ -3,23 +3,32 @@ fn main() {
     {
         use std::{env, path::PathBuf};
 
-        const LIB_NAME: &str = "CoolProp";
+        let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
+        let target_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
         println!("cargo:rerun-if-changed=build.rs");
         println!("cargo:rerun-if-changed=CoolPropLib.h");
-        let target_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-        let bindings = bindgen::Builder::default()
+
+        let mut builder = bindgen::Builder::default()
             .header("CoolPropLib.h")
             .derive_debug(true)
             .derive_default(true)
-            .dynamic_library_name(LIB_NAME)
-            .dynamic_link_require_all(true)
             .use_core()
             .generate_cstr(true)
             .generate_comments(false)
-            .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()));
+
+        // Only generate dynamic library dynamic pointers for non-WASM targets
+        if target_arch != "wasm32" {
+            builder = builder
+                .dynamic_library_name("CoolProp")
+                .dynamic_link_require_all(true);
+        }
+
+        let bindings = builder
             .generate()
             .expect("bindgen should generate bindings from `CoolPropLib.h`");
+
         bindings
             .write_to_file(target_dir.join("bindings.rs"))
             .expect("generated bindings should be written to `OUT_DIR`");
