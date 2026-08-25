a361ee0175f88e6abb53fbb36001e4aac7d80c1f
Sebastian Livoni <seb@ecaengineering.dk>
2026-08-25 12:53:23 +0200

    WIP: Static link option for rfluids

--- coolprop-sys/build.rs
+++ coolprop-sys/build.rs
@@ -14,30 +14,39 @@ fn main() {
         use std::{env, path::PathBuf};
 
         let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
-        let target_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+        let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
         println!("cargo:rerun-if-changed=build.rs");
         println!("cargo:rerun-if-changed=CoolPropLib.h");
 
-        let mut builder = bindgen::Builder::default()
-            .header("CoolPropLib.h")
-            .derive_debug(true)
-            .derive_default(true)
-            .use_core()
-            .generate_cstr(true)
-            .generate_comments(false)
-            .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()));
-
-        // Only generate dynamic library dynamic pointers for non-WASM targets
-        if target_arch != "wasm32" {
-            builder = builder.dynamic_library_name("CoolProp").dynamic_link_require_all(true);
+        fn base_builder() -> bindgen::Builder {
+            bindgen::Builder::default()
+                .header("CoolPropLib.h")
+                .derive_debug(true)
+                .derive_default(true)
+                .use_core()
+                .generate_cstr(true)
+                .generate_comments(false)
+                .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         }
 
-        let bindings =
-            builder.generate().expect("bindgen should generate bindings from `CoolPropLib.h`");
+        let static_bindings = base_builder()
+            .generate()
+            .expect("bindgen should generate static bindings from `CoolPropLib.h`");
 
-        bindings
-            .write_to_file(target_dir.join("bindings.rs"))
-            .expect("generated bindings should be written to `OUT_DIR`");
+        static_bindings
+            .write_to_file(out_dir.join("bindings_static.rs"))
+            .expect("static bindings should be written to `OUT_DIR`");
+
+        // Only generate dynamic library dynamic pointers for non-WASM targets
+        let dynamic_bindings = base_builder()
+            .dynamic_library_name("CoolProp")
+            .dynamic_link_require_all(true)
+            .generate()
+            .expect("bindgen should generate dynamic bindings from `CoolPropLib.h`");
+
+        dynamic_bindings
+            .write_to_file(out_dir.join("bindings_dynamic.rs"))
+            .expect("dynamic bindings should be written to `OUT_DIR`");
     }
 }
